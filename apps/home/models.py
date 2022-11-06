e03a60a7a5aab5017c8610762815cd2e463de26e
boomimjustin <jashtinshen0729@gmail.com>
2022-11-06 14:21:06 +0800

    models problem

--- apps/home/models.py
+++ apps/home/models.py
@@ -7,33 +7,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class EmergencyGenerators(models.Model):
-    id = models.IntegerField(max_length= 10, null=False)
-    period_starttime = models.DateField(null=False)
-    period_endttime = models.DateField(null=False)
-    device_id = models.CharField(max_length=30, null=False)
-    device_capacity = models.CharField(max_length=30, null=False)
-    capacity_unit = models.CharField(max_length=10, null=False)
-    position = models.CharField(max_length=30, null=False)
-    department = models.CharField(max_length=100, null=True)
-    total = models.FloatField(null=False)
-    fuel_unit = models.CharField(max_length=10, null=False)
-    image_path = models.CharField(max_length=100, null=False)
-    january = models.FloatField(null=False)
-    february = models.FloatField(null=False)
-    march = models.FloatField(null=False)
-    april = models.FloatField(null=False)
-    may = models.FloatField(null=False)
-    june = models.FloatField(null=False)
-    july = models.FloatField(null=False)
-    august = models.FloatField(null=False)
-    september = models.FloatField(null=False)
-    october = models.FloatField(null=False)
-    november = models.FloatField(null=False)
-    december = models.FloatField(null=False)
-
-    def _str_(self):
-        return self.id
+# class EmergencyGenerators(models.Model):
+#     id = models.IntegerField(max_length= 10, null=False)
+#     period_starttime = models.DateField(null=False)
+#     period_endttime = models.DateField(null=False)
+#     device_id = models.CharField(max_length=30, null=False)
+#     device_capacity = models.CharField(max_length=30, null=False)
+#     capacity_unit = models.CharField(max_length=10, null=False)
+#     position = models.CharField(max_length=30, null=False)
+#     department = models.CharField(max_length=100, null=True)
+#     total = models.FloatField(null=False)
+#     fuel_unit = models.CharField(max_length=10, null=False)
+#     image_path = models.CharField(max_length=100, null=False)
+#     january = models.FloatField(null=False)
+#     february = models.FloatField(null=False)
+#     march = models.FloatField(null=False)
+#     april = models.FloatField(null=False)
+#     may = models.FloatField(null=False)
+#     june = models.FloatField(null=False)
+#     july = models.FloatField(null=False)
+#     august = models.FloatField(null=False)
+#     september = models.FloatField(null=False)
+#     october = models.FloatField(null=False)
+#     november = models.FloatField(null=False)
+#     december = models.FloatField(null=False)
+#
+#     def _str_(self):
+#         return self.id
 
 
 
