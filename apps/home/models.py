6b8cf7f6ff37c6e5ac6abc17ce42ec5c59622c36
leo.shiu <leoshiujjf@gmail.com>
2022-11-14 17:57:25 +0800

    Change: models.py f1 & views.py function

--- apps/home/models.py
+++ apps/home/models.py
@@ -4,41 +4,21 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
-from django.contrib.auth.models import User, UserManager
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class section_one(models.Model):
-    cp_id = models.IntegerField(primary_key=True)
-    c_name = models.IntegerField
+    cp_id = models.AutoField(primary_key=True)
+    c_name = models.IntegerField()
     p_name = models.CharField(max_length=30)
 class section_two(models.Model):
-    d_id = models.IntegerField(primary_key=True)
+    d_id = models.AutoField(primary_key=True)
     d_name = models.CharField(max_length=30)
     cp_id = models.ForeignKey(section_one, on_delete=models.CASCADE)
-#
-# class GalaxyUser(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     create_time = models.DateTimeField(null=True, blank=True)
-#     update_time = models.DateTimeFi
-#     email = models.CharField(max_length=765)
-#     password = models.CharField(max_length=120)
-#     external = models.IntegerField(null=True, blank=True)
-#     deleted = models.IntegerField(null=True, blank=True)
-#     purged = models.IntegerField(null=True, blank=True)
-#     username = models.CharField(max_length=765, blank=True)
-#     form_values_id = models.IntegerField(null=True, blank=True)
-#     disk_usage = models.DecimalField(null=True, max_digits=16, decimal_places=0, blank=True)
-#
-#     objects = UserManager()
-#
-#     class Meta:
-#         db_table = u'galaxy_user'
-
-
 class emergency_generators(models.Model):
-    id = models.IntegerField(primary_key=True,)
-    d_id = models.ForeignKey(section_two, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True,)
+    d_id = models.ForeignKey(section_two, on_delete=models.CASCADE, default=1)
     device_id = models.CharField(max_length=30)
     period_starttime = models.DateField()
     period_endttime = models.DateField()
--- apps/home/views.py
+++ apps/home/views.py
@@ -144,17 +144,18 @@ def emergency_generator(request):
 
 @login_required(login_url="/login/")
 def carbon_system(request):
-    # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>0")
-    #
-    # if request.method == 'GET':
-    #     currentClass = request.GET.get('cclass')
-    #     if currentClass == None:
-    #         currentClass = 0
-    #     print(">>>>>>>>>>>>>>>>>>>>>.", currentClass)
-    #     allProcess = section_one.objects.filter(c_name=currentClass).values()
-    #     # allProcess = section_one.objects.filter(c_name='1')
-    #     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>1")
-    #     # selectProcess = allProcess.p_name
-    #     # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>2",selectProcess)
-    #     context = {'allProcess': allProcess}
-    return render(request, "home/carbon-system.html", locals())
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>0")
+
+    if request.method == 'GET':
+        currentClass = request.GET.get('cclass')
+        if currentClass == None:
+            currentClass = 0
+        print(">>>>>>>>>>>>>>>>>>>>>.", currentClass)
+        aa = section_one.objects.all()
+        allProcess = section_one.objects.filter(c_name=currentClass).values()
+        # allProcess = section_one.objects.filter(c_name='1')
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>1")
+        # selectProcess = allProcess.p_name
+        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>2",selectProcess)
+        context = {'allProcess': allProcess}
+    return render(request, "home/carbon-system.html", context)
