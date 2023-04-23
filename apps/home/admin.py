d85bd54902c91c1aad202a1a47f2ff86cc25968a
leo.shiu <leoshiujjf@gmail.com>
2023-04-24 02:00:09 +0800

    Change: admin can create company

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,15 +1,9 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.contrib import admin
-
 from .forms import CompanyForm
 from .models import *
 
 
-# Register your models here.
 @admin.register(company)
 # admin.site.register(company, CompanyAdmin)
 class CompanyAdmin(admin.ModelAdmin):
