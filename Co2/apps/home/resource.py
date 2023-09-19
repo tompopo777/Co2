e9bc1c594ded12dbd13b32c6241100eed11ad07f
boomimjustin <45955206+boomimjustin@users.noreply.github.com>
2023-09-19 18:42:34 +0800

    Delete Co2 directory

--- Co2/apps/home/resource.py
+++
@@ -1,7 +0,0 @@
-# from import_export import resources
-# from .models import *
-#
-#
-# class EmergencyGeneratorsResource(resources.ModelResource):
-#     class Meta:
-#         model = emergency_generators
--- Co2/core/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-import pymysql
-pymysql.install_as_MySQLdb()
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
